0272442f048b3a2ae37186fbcabcf541b32cc635
manuelmenama <manuel.mena090589@gmail.com>
2022-10-11 17:19:42 +0200

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const tripLenght = 30;
-const userAge = 10;
+const tripLenght = prompt('Distanza da percorrere (in km):')
+const userAge = prompt('Scrivi qui la tua età:')
 const kilometerPrice = 0.21;
 const youngDiscount = 0.2;
 const agedDiscount = 0.4;
@@ -12,12 +12,27 @@ console.log('Prezzo pieno:', pricePerKilometer);
 
 if(userAge >= 65){
     let agedPrice = pricePerKilometer - pricePerKilometer * agedDiscount;
-    console.log('Prezzo per over 65:', agedPrice);
+    let agedPriceFixed = agedPrice.toFixed(2);
+    document.getElementById('text').innerHTML = `
+        Prezzo con sconto del ${agedDiscount * 100}% per over 65: ${agedPriceFixed}
+    `;
+
+    console.log('Prezzo per over 65:', agedPriceFixed);
 }else if(userAge <= 18){
     let youngPrice = pricePerKilometer - pricePerKilometer * youngDiscount;
-    console.log('Prezzo per under 18:', youngPrice);
+    let youngPriceFixed = youngPrice.toFixed(2);
+    document.getElementById('text').innerHTML = `
+    Prezzo con sconto del ${youngDiscount * 100}% per under 18: ${agedPrice}
+`;
+    
+    console.log('Prezzo per under 18:', youngPriceFixed);
 }else{
-    console.log('Prezzo pieno:', pricePerKilometer);
+
+    let pricePerKilometerFixed = pricePerKilometer.toFixed(2);
+    document.getElementById('text').innerHTML = `
+        Il prezzo del suo biglietto è: ${pricePerKilometerFixed}
+    `;
+    console.log('Prezzo pieno:', pricePerKilometerFixed);
 }
 
 
