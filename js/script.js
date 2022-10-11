5e15c8ee38246d8486644dbafbdc28bb56c383f6
manuelmenama <manuel.mena090589@gmail.com>
2022-10-11 15:52:13 +0200

    some changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('hello js');
+const tripLenght = 30;
+const userAge = 40;
+const kilometerPrice = 0.21;
+const youngDiscount = 0.2;
+const agedDiscount = 0.4;
+let youngPrice;
+let agedPrice;
+
+const pricePerKilometer = tripLenght * kilometerPrice;
+
+
+console.log('Lunghezza del viaggio:', tripLenght);
+console.log('Età del passeggero:', userAge);
+console.log('Prezzo pieno:', pricePerKilometer);
+
