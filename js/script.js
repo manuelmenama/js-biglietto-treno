9c127c0079aad02e36140f301906331c2b979906
manuelmenama <manuel.mena090589@gmail.com>
2022-10-11 16:18:32 +0200

    ended calc

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 const tripLenght = 30;
-const userAge = 40;
+const userAge = 10;
 const kilometerPrice = 0.21;
 const youngDiscount = 0.2;
 const agedDiscount = 0.4;
-let youngPrice;
-let agedPrice;
 
 const pricePerKilometer = tripLenght * kilometerPrice;
-
 
 console.log('Lunghezza del viaggio:', tripLenght);
 console.log('Età del passeggero:', userAge);
 console.log('Prezzo pieno:', pricePerKilometer);
+
+if(userAge >= 65){
+    let agedPrice = pricePerKilometer - pricePerKilometer * agedDiscount;
+    console.log('Prezzo per over 65:', agedPrice);
+}else if(userAge <= 18){
+    let youngPrice = pricePerKilometer - pricePerKilometer * youngDiscount;
+    console.log('Prezzo per under 18:', youngPrice);
+}else{
+    console.log('Prezzo pieno:', pricePerKilometer);
+}
+
 
